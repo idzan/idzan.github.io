779d2e8954b36e88b7d5f4af41abecc15355de79
Marko Idzan <marko@idzan.eu>
2018-08-17 22:31:13 +0200

    So much edits
    So mucho tweaks in design and Navigation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,6 @@
 (function ($) {
     "use strict";
-
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.lenght ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.lenght) {
-                $('html', 'body').animate({
-                    scrolltop: (target.offset().top - 70)
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
-
-    $('js-scroll-trigger').click(function () {
-        $('navbar-collapse').collapse('hide');
-    });
-
+    
     $('body').scrollspy({
         target: '#mainNav',
         offset: 100
