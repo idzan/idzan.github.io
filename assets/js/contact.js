939d0d8ea69640724084aef3157de6a62f692388
Marko Idzan <42409619+idzan@users.noreply.github.com>
2019-06-21 14:01:45 +0200

    Ajax revision test
    Hopefully it works fine :fire:

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,45 +1,22 @@
-const form = document.getElementById('contactMe')
-const url = 'https://w93b5g7u0g.execute-api.eu-west-1.amazonaws.com/dev/email/send'
-const infoDetails = document.getElementById('infoBox')
-const submit = document.getElementById('submit')
-function post(url, body, callback) {
-  var req = new XMLHttpRequest();
-  req.open("POST", url, true);
-  req.setRequestHeader("Content-Type", "application/json");
-  req.addEventListener("load", function () {
-    if (req.status < 400) {
-      callback(null, JSON.parse(req.responseText));
-    } else {
-      callback(new Error("Request failed: " + req.statusText));
-    }
-  });
-  req.send(JSON.stringify(body));
-}
-function success () {
-  infoDetails.innerHTML = 'Thanks for sending me a message! I\'ll get in touch with you.'
-  submit.disabled = false
-  submit.blur()
-  form.name.focus()
-  form.name.value = ''
-  form.email.value = ''
-  form.content.value = ''
-}
-function error (err) {
-  infoDetails.innerHTML = 'There was an error with sending your message, hold up until I fix it. Thanks for waiting.'
-  submit.disabled = false
-  console.log(err)
-}
-form.addEventListener('submit', function (e) {
-  e.preventDefault()
-  infoDetails.innerHTML = 'Sending'
-  submit.disabled = true
-  const payload = {
-    name: form.name.value,
-    email: form.email.value,
-    content: form.content.value
-  }
-  post(url, payload, function (err, res) {
-    if (err) { return error(err) }
-    success()
-  })
+let contactForm = document.querySelector("contactMe");
+let infoDetails = document.querySelector("infoBox");
+
+
+contactForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(contactForm);
+    fetch(contactForm.getAttribute('action'), {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Content-Type': 'application/x-www-form-urlencoded;charset="UTF-8'
+        },
+        body: new URLSearchParams(formData).toString()
+    })
+    .then(res => {
+        if(res) {
+            infoDetails.innerHTML = 'Thanks for sending me a message! I\'ll get in touch with you.'
+        }
+    })
 })
